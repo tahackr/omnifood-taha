7ea6acad706d583b630e1c18ad671ebb2b8717a9
tahackr <tahacakir@hotmail.com>
2023-01-31 01:40:05 +0300

    modified event listener and added sticky navigation feature

--- script.js
+++ script.js
@@ -1,70 +1,119 @@
 "use strict";
-// Event Listeners for scrolling
+
 (function () {
   const headerEl = document.querySelector(".header");
-  const section1 = document.querySelector(".section-how");
-  const btnLearnMore = document.querySelector(".btn--outline");
 
-  // Learn more button event listener
-
-  btnLearnMore.addEventListener("click", function (e) {
-    e.preventDefault();
-
-    const coords = section1.getBoundingClientRect();
-
-    window.scrollTo({
-      top: coords.top + window.scrollY,
-      left: coords.left + window.scrollX,
-      behavior: "smooth",
-    });
-  });
-
-  // Header event listener for logo and nav bar
-
-  headerEl.addEventListener("click", function (e) {
-    e.preventDefault();
-    // Header, main-nav-list("UL"), main-nav("nav") guard clause
-    if (
-      e.target === document.querySelector(".header") ||
-      e.target === document.querySelector(".main-nav-list") ||
-      e.target === document.querySelector(".main-nav")
-    )
-      return;
-
-    // Mobile nav button guard clause
-    if (e.target.closest(".btn-mobile-nav")) return;
-
-    // Logo function
-    if (
-      e.target === document.querySelector(".logo") ||
-      e.target === document.querySelector(".logo").closest("a")
-    ) {
-      /* 
-      *****************************
-      Sticky class varsa reload yerine sayfanın yukarısına gitme özelliği ekle
-      *****************************
-      */
-      location.reload();
-      return;
-    }
-
-    // Nav bar function for both mobile and desktop
-    const id = e.target.closest(".main-nav-link").getAttribute("href");
-    const section = document.querySelector(id);
-    const coords = section.getBoundingClientRect();
-    window.scrollTo({
-      top: coords.top + window.scrollY,
-      left: coords.left + window.scrollX,
-      behavior: "smooth",
-    });
-
-    headerEl.classList.remove("nav-open");
-  });
+  // Updating the year that is in the footer
+  const yearEl = document.querySelector(".year");
+  const present = new Date().getFullYear();
+  yearEl.textContent = present;
 
   // Event listener for toggling mobile nav bar
   const mobileButton = document.querySelector(".btn-mobile-nav");
 
   mobileButton.addEventListener("click", function (e) {
     headerEl.classList.toggle("nav-open");
+
+    // Disabling and enabling scroll
+    document.body.classList.toggle("noscrl");
+    document.querySelector("html").classList.toggle("noscrl");
   });
+
+  // Smooth scrolling for all the links
+  const body = document.querySelector("body");
+  body.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    if (
+      e.target.classList.contains("logo") &&
+      e.target.parentElement.getAttribute("href")
+    ) {
+      !headerEl.classList.contains("sticky")
+        ? location.reload()
+        : window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+          });
+    }
+
+    // Navigation for both mobile and desktop
+    if (e.target.closest(".main-nav-link")) {
+      const id = e.target.closest(".main-nav-link").getAttribute("href");
+      const coords = document.querySelector(id).getBoundingClientRect();
+      window.scrollTo({
+        top: coords.top + window.scrollY,
+        behavior: "smooth",
+      });
+      headerEl.classList.remove("nav-open");
+      document.body.classList.remove("noscrl");
+      document.querySelector("html").classList.remove("noscrl");
+      return;
+    }
+
+    // Guard clause
+    if (!e.target.getAttribute("href")) return;
+
+    // Other anchors on the page
+    if (
+      e.target.getAttribute("href").startsWith("#") &&
+      e.target.getAttribute("href").length > 1
+    ) {
+      const id = e.target.getAttribute("href");
+      const coords = document.querySelector(id).getBoundingClientRect();
+      window.scrollTo({
+        top: coords.top + window.scrollY - 50,
+        behavior: "smooth",
+      });
+    }
+
+    if (e.target.getAttribute("href") === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+  });
+
+  // Sticky navigation
+  const observer = new IntersectionObserver(
+    function (entries) {
+      const [entry] = entries;
+
+      if (!entry.isIntersecting) {
+        headerEl.classList.add("sticky");
+        document.querySelector(".section-hero").style.marginTop = "9.6rem";
+      }
+
+      if (entry.isIntersecting) {
+        headerEl.classList.remove("sticky");
+        document.querySelector(".section-hero").style.marginTop = "";
+      }
+    },
+    {
+      root: null,
+      threshold: 0,
+      rootMargin: `-64px`,
+    }
+  );
+  observer.observe(document.querySelector(".section-hero"));
+
+  ///////////////////////////////////////////////////////////
+  // Fixing flexbox gap property missing in some Safari versions
+  function checkFlexGap() {
+    var flex = document.createElement("div");
+    flex.style.display = "flex";
+    flex.style.flexDirection = "column";
+    flex.style.rowGap = "1px";
+
+    flex.appendChild(document.createElement("div"));
+    flex.appendChild(document.createElement("div"));
+
+    document.body.appendChild(flex);
+    var isSupported = flex.scrollHeight === 1;
+    flex.parentNode.removeChild(flex);
+    console.log(isSupported);
+
+    if (!isSupported) document.body.classList.add("no-flexbox-gap");
+  }
+  checkFlexGap();
 })();
